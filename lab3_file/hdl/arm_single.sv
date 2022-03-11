999595f06dd6b922cc6cfcda83d6b3c598d3f984
RHoepnerOSU <Richard.Hoepner@my.okstate.edu>
2022-03-11 14:32:02 -0600

    original code

--- lab3_file/hdl/arm_single.sv
+++ lab3_file/hdl/arm_single.sv
@@ -380,25 +380,7 @@ module regfile (input  logic        clk,
                 output logic [31:0] rd1, rd2);
    
    logic [31:0] rf[14:0];
-  initial
-  begin   //start of our regfile
-	  for(int i=0; i <=31; i++)
-	  begin
-		  rf[i] = 0;
-    end
-  end
-    // three ported register file
-    // read two ports combinationally
-  always_comb 
-  begin
-    rd1 = rf[ra1];
-    rd2 = rf[ra2];
-  end
-    // write third port on rising edge of clock
-    always @(posedge clk)
-  begin	
-    rf[wa3] = (we3 && wa3 != 0) ? wd3  : rf[wa3];
-  end  // end of our regfile
+ 
    // three ported register file
    // read two ports combinationally
    // write third port on rising edge of clock
